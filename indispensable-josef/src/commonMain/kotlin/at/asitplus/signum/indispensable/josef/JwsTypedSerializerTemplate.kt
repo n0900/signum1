d69d9692f8b52604b59e62dbe6f746754def4bb7
Simon Mueller <simon.mueller@a-sit.at>
2026-08-25 13:26:10 +0200

    Implement typed serializer

--- indispensable-josef/src/commonMain/kotlin/at/asitplus/signum/indispensable/josef/JwsTypedSerializerTemplate.kt
+++ indispensable-josef/src/commonMain/kotlin/at/asitplus/signum/indispensable/josef/JwsTypedSerializerTemplate.kt
@@ -19,26 +19,49 @@ class JwsTypedSerializerTemplate<J : JWS, P, H : JwsHeaderBase>(
     decodeAs = { jws ->
         with(joseCompliantSerializer) {
             when (jws) {
-                is JwsCompact -> JwsCompactTyped(
-                    jws,
-                    jws.getPayload(payloadSerializer).getOrThrow(),
-                    JwsHeaderWrapped.fromParts(headerSerializer, jws.plainProtectedHeader, null)
-                )
-
-                is JwsFlattened -> JwsFlattenedTyped(
-                    jws, jws.getPayload(payloadSerializer).getOrThrow(),
-                    JwsHeaderWrapped.fromParts(headerSerializer, jws.plainProtectedHeader, jws.unprotectedHeader)
-                )
-
-                is JwsGeneral -> JwsGeneralTyped(
-                    jws, jws.getPayload(payloadSerializer).getOrThrow(),
-                    jws.signatureElements.map {
-                        JwsHeaderWrapped.fromParts(
-                            headerSerializer,
-                            it.plainProtectedHeader,
-                            it.unprotectedHeader
+                is JwsCompact ->
+                    JwsHeaderWrapped.fromParts(headerSerializer, jws.plainProtectedHeader, null).let { wrapped ->
+                        JwsCompactTyped(
+                            jws,
+                            jws.getPayload(payloadSerializer).getOrThrow(),
+                            wrapped,
+                            JWS.getSignature(wrapped.header.algorithm, jws.plainSignature),
                         )
-                    })
+                    }
+
+                is JwsFlattened ->
+                    JwsHeaderWrapped.fromParts(
+                        headerSerializer,
+                        jws.plainProtectedHeader,
+                        jws.unprotectedHeader,
+                    ).let { wrapped ->
+                        JwsFlattenedTyped(
+                            jws,
+                            jws.getPayload(payloadSerializer).getOrThrow(),
+                            wrapped,
+                            JWS.getSignature(wrapped.header.algorithm, jws.plainSignature),
+                        )
+                    }
+
+                is JwsGeneral -> jws.signatureElements.map {
+                    JwsHeaderWrapped.fromParts(
+                        headerSerializer,
+                        it.plainProtectedHeader,
+                        it.unprotectedHeader,
+                    )
+                }.let { wrappedHeaders ->
+                    JwsGeneralTyped(
+                        jws,
+                        jws.getPayload(payloadSerializer).getOrThrow(),
+                        wrappedHeaders,
+                        wrappedHeaders.zip(jws.signatureElements) { wrapped, signatureElement ->
+                            JWS.getSignature(
+                                wrapped.header.algorithm,
+                                signatureElement.plainSignature,
+                            )
+                        },
+                    )
+                }
             } as JwsTyped<J, P, H>
         }
     }
